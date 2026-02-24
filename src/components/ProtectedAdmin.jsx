d02b77995d98f53bdc4d6760c7a23e4b9d5585a2
G H O S T <154050934+GHOST-root@users.noreply.github.com>
2026-02-24 12:34:05 +0500

    Update ProtectedAdmin.jsx

--- src/components/ProtectedAdmin.jsx
+++ src/components/ProtectedAdmin.jsx
@@ -1,7 +1,3 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-
 function ProtectedAdmin({ children }) {
   return children;
 }
