78282ac77e4563eea673fe2dcff34869fa388ecd
G H O S T <154050934+GHOST-root@users.noreply.github.com>
2026-02-24 12:30:43 +0500

    Refactor ProtectedAdmin component
    Removed admin check logic from ProtectedAdmin component.

--- src/components/ProtectedAdmin.jsx
+++ src/components/ProtectedAdmin.jsx
@@ -3,16 +3,6 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 function ProtectedAdmin({ children }) {
-  const { isAdmin } = useContext(AuthContext);
-
-  if (isAdmin === null) {
-    return null; // yoki loader
-  }
-
-  if (!isAdmin) {
-    return <Navigate to="/admin-login" replace />;
-  }
-
   return children;
 }
 
